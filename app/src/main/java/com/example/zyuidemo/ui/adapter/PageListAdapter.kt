a63158fca845441583e575ca6736481cc2215b41
张永 <uSGzYuXM5iMERnXT>
2021-04-25 14:29:19 +0800

    测试查看大图按钮

--- app/src/main/java/com/example/zyuidemo/ui/adapter/PageListAdapter.kt
+++ app/src/main/java/com/example/zyuidemo/ui/adapter/PageListAdapter.kt
@@ -4,6 +4,8 @@ import android.graphics.drawable.Animatable
 import android.net.Uri
 import android.view.View
 import androidx.core.view.ViewCompat
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import com.blankj.utilcode.util.ScreenUtils
 import com.example.zyuidemo.R
 import com.example.zyuidemo.base.multitype.vu.BaseVu
@@ -13,9 +15,10 @@ import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.drawee.controller.BaseControllerListener
 import com.facebook.drawee.drawable.ScalingUtils
 import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder
+import com.facebook.imagepipeline.common.Priority
 import com.facebook.imagepipeline.common.ResizeOptions
 import com.facebook.imagepipeline.image.ImageInfo
-import com.facebook.imagepipeline.request.ImageRequest
+import com.facebook.imagepipeline.postprocessors.IterativeBoxBlurPostProcessor
 import com.facebook.imagepipeline.request.ImageRequestBuilder
 import com.facebook.imageutils.BitmapUtil
 
@@ -31,22 +34,44 @@ class PageListView : BaseVu<LayoutPictureBinding, PictureItem>(), View.OnClickLi
 
     override fun bindData(data: PictureItem) {
         mdata = data
+        handleImage(data)
+        binding.tvPreviewBig.isGone = mdata.isPreviewBig == true
+        binding.tvPreviewBig.setOnClickListener(this)
+    }
 
+    private fun handleImage(data: PictureItem) {
         binding.picture?.let {
             it.isEnableDraweeMatrix = false
             val url = data.url
             it.tag = adapterPosition
-            val request = ImageRequestBuilder.newBuilderWithSource(Uri.parse("$url?x-oss-process=image/resize,w_400/quality,Q_70"))
-                    .setResizeOptions(ResizeOptions(ScreenUtils.getAppScreenWidth(), ScreenUtils.getAppScreenHeight(), BitmapUtil.MAX_BITMAP_SIZE * 10))
+            val lowResImageRequest = ImageRequestBuilder.newBuilderWithSource(Uri.parse(url))
+                .setResizeOptions(ResizeOptions(10, 10))
+                .setRequestPriority(Priority.LOW)
+                .setLocalThumbnailPreviewsEnabled(true)
+                .setPostprocessor(IterativeBoxBlurPostProcessor(10, 10))
+                .build()
+            val smallRequest = ImageRequestBuilder.newBuilderWithSource(Uri.parse(url))
+                .setResizeOptions(ResizeOptions(100, 100))
+                .setRequestPriority(Priority.LOW)
+                .build()
+            val bigRequest =
+                ImageRequestBuilder.newBuilderWithSource(Uri.parse("$url?x-oss-process=image/resize,w_400/quality,Q_70"))
+                    .setResizeOptions(
+                        ResizeOptions(
+                            ScreenUtils.getAppScreenWidth(),
+                            ScreenUtils.getAppScreenHeight(),
+                            BitmapUtil.MAX_BITMAP_SIZE * 10
+                        )
+                    )
                     .build()
             val hierarchy = GenericDraweeHierarchyBuilder.newInstance(it.resources)
-                    .setFailureImageScaleType(ScalingUtils.ScaleType.CENTER)
-                    .setActualImageScaleType(ScalingUtils.ScaleType.FIT_CENTER)
-                    .setFailureImage(R.drawable.picture_icon_data_error)
+                .setFailureImageScaleType(ScalingUtils.ScaleType.CENTER)
+                .setActualImageScaleType(ScalingUtils.ScaleType.FIT_CENTER)
+                .setFailureImage(R.drawable.picture_icon_data_error)
             ViewCompat.setTransitionName(it, url + adapterPosition)
             val controller = Fresco.newDraweeControllerBuilder()
-            controller.imageRequest = request
-            controller.lowResImageRequest = ImageRequest.fromUri("$url?x-oss-process=image/resize,w_50/quality,Q_30")
+            controller.imageRequest = if (data.isPreviewBig == true) bigRequest else smallRequest
+            controller.lowResImageRequest = lowResImageRequest
             controller.autoPlayAnimations = true
             controller.oldController = it.controller
             controller.controllerListener = object : BaseControllerListener<ImageInfo?>() {
@@ -66,7 +91,11 @@ class PageListView : BaseVu<LayoutPictureBinding, PictureItem>(), View.OnClickLi
                     super.onFailure(id, throwable)
                 }
 
-                override fun onFinalImageSet(id: String, imageInfo: ImageInfo?, animatable: Animatable?) {
+                override fun onFinalImageSet(
+                    id: String,
+                    imageInfo: ImageInfo?,
+                    animatable: Animatable?
+                ) {
                     super.onFinalImageSet(id, imageInfo, animatable)
                     if (imageInfo == null) {
                         return
@@ -84,7 +113,12 @@ class PageListView : BaseVu<LayoutPictureBinding, PictureItem>(), View.OnClickLi
     }
 
     override fun onClick(v: View?) {
-        mVuCallBack?.onCallBack(mdata, getAdapterPos())
+        if (v?.id == R.id.tv_preview_big) {
+            mdata.isPreviewBig = !mdata.isPreviewBig!!
+            bindData(mdata)
+        } else {
+            mVuCallBack?.onCallBack(mdata, getAdapterPos())
+        }
     }
 
     override fun onViewTap(view: View?, x: Float, y: Float) {
@@ -94,5 +128,6 @@ class PageListView : BaseVu<LayoutPictureBinding, PictureItem>(), View.OnClickLi
 
 data class PictureItem(
     val id: Int,
-    val url: String
+    val url: String,
+    var isPreviewBig: Boolean? = false
 )
